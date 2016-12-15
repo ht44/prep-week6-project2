7fec9a6a4c7fd428bc32e63582c4b9a2fd499361
ht44 <haydenturek@gmail.com>
2016-12-15 14:46:47 -0600

    first commit

--- main.js
+++ main.js
@@ -11,42 +11,64 @@
 // Add your JS here.
 
 
-$(document).ready(function() {  
-    
-    
-//1. 
-   
-   
-//2.  
-    
-    
-//3. 
-   
-	    
-//4.
- 
-   
-//5. 
+$(document).ready(function() {
 
-	
+
+//1.
+
+$(".alertme").click(function() {
+  alert("jQuery seems easy so far!");
+});
+
+//2.
+
+$(".clickme").click(function() {
+  $(".clickme").text("I was clicked");
+});
+
+//3.
+
+$(".addStyle").click(function() {
+  $("button").addClass("buttonStyle");
+});
+
+//4.
+
+$(".addDifferentStyle").click(function() {
+  $(".addDifferentStyle").addClass("addUniqueStyle");
+});
+
+//5.
+
+$(".removeBtn").click(function() {
+  $("button").removeClass("buttonStyle");
+  $("button").removeClass("addUniqueStyle");
+});
+
 //6.
-	
-	
+
+$(".clickToggle").click(function() {
+  $(".clickToggle").toggleClass("buttonStyle");
+});
+
 //7.
 
-	
-	
+$(".squareBtn").click(function() {
+  $(".square").hide();
+});
+
 //8.
 
+$(".circle").click(function() {
+  $(".circle").hide();
+});
 
 
 //9.
 
+$(".toggleRedSquare").click(function() {
+  $(".redSquare").toggle(1500);
+});
 
 
-});  // Close: $(document).ready(function() { 
-	
-	
-
-	
-	
+});  // Close: $(document).ready(function() {
